7a77b973e2881dedd2d8fd0d294b9abaa58bdf8e
Tim Stella <denstark@gmail.com>
2012-04-03 20:26:03 -0700

    New queue system, album queueing working
    Lots of changes. Implemented a helper function and now the queue is handled
    within the helper program instead of by mplayer.
    
    Queueing songs is working
    Queueing albums is working
    Clearing the queue is working
    
    There is very little in the way of error checking. Bad things might happen if
    you enter in a wrong value somewhere.
    
    Also, this uses shared memory, which means that /etc/php/php.ini must load the
    following extensions:
    
    extension=sysvmsg.so
    extension=sysvsem.so
    extension=sysvshm.so
    
    Otherwise things don't work.
    
    Up next is next, prev, and repeat, and maybe some UI fixes.

--- functions.php
+++ functions.php
@@ -11,9 +11,7 @@ function createFifo($mplayerfifo)
 function startMplayer()
 {
   global $mplayerfifo, $cachesize;
-  echo "starting mpalyer\n";
   exec("mplayer -really-quiet -cache $cachesize -slave -input file=$mplayerfifo -idle > /dev/null 2>&1 &", $output);
-  echo "mplayer started!\n";
 } // startMplayer function
 
 function getArtists()
@@ -58,17 +56,25 @@ function getMusicDir($mdirid)
     $title = (string)$entry->attributes()->title;
     $id = (string)$entry->attributes()->id;
     $isDir = (string)$entry->attributes()->isDir;
-    if ($isDir) 
+    $duration = (int)$entry->attributes()->duration;
+    $album = (string)$entry->attributes()->album;
+    $isDir = ($isDir == 'true');
+    if ($isDir)
     {
       $entries_arr[$count] = array('title' => $title, 'id' => $id, 'isDir' => $isDir);
     } else // It's file, do file things
     {
-
+      $entries_arr[$count] = array(
+        'title'     => $title,
+        'id'        => $id,
+        'isDir'     => $isDir,
+        'duration'  => $duration,
+        'album'     => $album,
+        'p'         => 0,
+      );
     }
     echo "$count: $title\n";
     $count++;
-
-    //print_r($entry);
   }
   return $entries_arr;
 
@@ -80,13 +86,16 @@ function loadSong($id)
   $mp3file = "$mp3dir/$id.mp3";
   exec("curl -ksN '$baseurl/stream.view$dopts&id=$id' -o $mp3file > /dev/null 2>&1 &");
   if ($songcount == 0)
-    sleep(2);
+    sleep(1);
   system('pgrep -f ".*mplayer.*clisonic.*"', $isrunning);
   if ($isrunning == 0)
   {
     // Mplayer is running
-    system("echo 'loadfile $mp3file $songcount' >> $mplayerfifo");
-    $songcount++;
+    // system("echo 'loadfile $mp3file $songcount' >> $mplayerfifo"); // 
+    // Needs to be reimplemented when I figure out a solution to queuing from 
+    // $x seconds behind the current song
+    system("echo 'loadfile $mp3file' >> $mplayerfifo");
+    // $songcount++;
   } else 
   {
     exit("Mplayer wasn't running for some reason");
@@ -94,8 +103,52 @@ function loadSong($id)
 
 } // loadSong function
 
+function addToQueue()
+{
+  global $artists_arr, $stdio, $queue, $shmem;
+  $isDir = true;
+  printArtists($artists_arr);
+  echo "Please input an artist number: ";
+  $artist_index = trim(fgets($stdio));
+  $mdirid = $artists_arr[$artist_index]['id'];
 
+  while ($isDir)
+  {
+    $entries_arr = getMusicDir($mdirid);
+    echo "Please input an album/track number (A to queue the album): ";
+    $album_index = trim(fgets($stdio));
+    if ( strtolower($album_index) == 'a')
+    {
+      $queue = shm_get_var($shmem, SHM_QUEUE_KEY);
+      foreach ($entries_arr as $entry)
+        $queue[] = $entry;
+      shm_put_var($shmem, SHM_QUEUE_KEY, $queue);
+      break 1;
+    } 
+    $isDir = $entries_arr[$album_index]['isDir'];
+    $mdirid = $entries_arr[$album_index]['id'];
+  } 
+  // We now are broken out of the selection loop and need to add the song to the 
+  // queue
+  if (strtolower($album_index) != 'a')
+  {
+    $queue = shm_get_var($shmem, SHM_QUEUE_KEY);
+    $queue[] = $entries_arr[$album_index];
+    shm_put_var($shmem, SHM_QUEUE_KEY, $queue);
+  }
+  // loadSong($mdirid);
 
+} // addToQueue function
+
+function clearQueue()
+{
+  global $shmem, $queue, $mplayerfifo;
+  $queue = array();
+  shm_put_var($shmem, SHM_QUEUE_KEY, $queue);
+  $timeLeft = 0;
+  shm_put_var($shmem, SHM_TIMELEFT_KEY, $timeLeft);
+  exec("echo 'stop' >> $mplayerfifo");
+} // clearQueue function
 
 
 ?>
